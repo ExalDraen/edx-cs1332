6d4a56514714f74be434e2ef5f7227fad5324f2c
Alexander Hermes <alexander.hermes@alnx.org>
2022-07-16 23:12:47 +0800

    Mod 2: Singly Linked List Impl

---
+++ src/org/alnx/edx/mod2/Main.java
@@ -0,0 +1,119 @@
+package org.alnx.edx.mod2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        test_0();
+        test_0_1();
+        test_1();
+        test_2();
+        test_3();
+        test_4();
+        test_5();
+    }
+
+    private static void test_5() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<String>();
+        arr.addToBack("1");
+        arr.addToBack("2");
+        arr.addToBack("3");
+        arr.addToBack("4");
+        arr.addToBack("5");
+        arr.addToBack("6");
+        System.out.printf("Removed: %s\n", arr.removeFromBack());
+        printLinkedList(arr);
+    }
+
+    private static void test_4() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<String>();
+        arr.addToBack("1");
+        arr.addToBack("2");
+        arr.addToBack("3");
+        arr.addToBack("4");
+        arr.addToBack("5");
+        arr.addToBack("6");
+        arr.addToBack("7");
+        arr.addToBack("8");
+        arr.addToBack("9");
+        printLinkedList(arr);    }
+
+    private static void test_3() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<String>();
+        arr.addToFront("Unique");
+        System.out.printf("Removed: %s\n", arr.removeFromFront());
+        printLinkedList(arr);    }
+
+    private static void test_2() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<String>();
+        arr.addToBack("There");
+        arr.addToBack("Was");
+        arr.addToBack("Once");
+        arr.addToBack("A");
+        arr.addToBack("Lady");
+        arr.addToBack("From");
+        arr.addToBack("Riga");
+        arr.addToBack(",");
+        arr.addToBack("Who");
+        arr.addToBack("Liked");
+        arr.addToBack("To");
+        arr.addToBack("Ride");
+        arr.addToBack("Out");
+        arr.addToBack("On");
+        arr.addToBack("A");
+        printLinkedList(arr);    }
+
+    private static void test_1() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<String>();
+        arr.addToBack("My");
+        arr.addToBack("Little");
+        arr.addToBack("Pony");
+        arr.addToBack(",");
+        arr.addToBack("Friendship");
+        printLinkedList(arr);
+        System.out.printf("Removed: %s\n", arr.removeFromFront());
+        System.out.printf("Removed: %s\n", arr.removeFromFront());
+        System.out.printf("Removed: %s\n", arr.removeFromFront());
+        System.out.printf("Removed: %s\n", arr.removeFromFront());
+        System.out.printf("Removed: %s\n", arr.removeFromFront());
+        printLinkedList(arr);
+    }
+
+    private static void test_0_1() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<Integer>();
+        arr.addToFront(1);
+        printLinkedList(arr);
+    }
+
+    private static void test_0() {
+        System.out.println("-----------------------------------");
+        var arr = new SinglyLinkedList<Integer>();
+        arr.addToFront(1);
+        arr.addToFront(2);
+        arr.addToFront(3);
+        arr.addToFront(4);
+        printLinkedList(arr);
+    }
+
+    private static <T> void printLinkedList(SinglyLinkedList<T> list) {
+        var curr = list.getHead();
+
+        var out = new StringBuilder();
+        out.append("[ ");
+        while (curr != null) {
+            out.append(curr.getData().toString());
+            out.append(", ");
+            curr = curr.getNext();
+        }
+        out.append(" ]");
+        out.append(" (size: ").append(list.size()).append(")");
+        out.append(" (head: ").append(list.getHead()).append(")");
+        out.append(" (tail: ").append(list.getTail()).append(")");
+        System.out.println(out);
+    }
+}
---
+++ src/org/alnx/edx/mod2/SinglyLinkedList.java
@@ -0,0 +1,173 @@
+package org.alnx.edx.mod2;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of a Singly-Linked List.
+ */
+public class SinglyLinkedList<T> {
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private SinglyLinkedListNode<T> head;
+    private SinglyLinkedListNode<T> tail;
+    private int size;
+
+    /*
+     * Do not add a constructor.
+     */
+
+    /**
+     * Adds the element to the front of the list.
+     * <p>
+     * Method should run in O(1) time.
+     *
+     * @param data the data to add to the front of the list
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addToFront(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Can't add null data!");
+        }
+        size++;
+        var newNode = new SinglyLinkedListNode<T>(data);
+        // special case: size was 0 before (now 1)
+        if (size == 1){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode.setNext(head);
+        head = newNode;
+        // tail pointer edge case, for adding to a list to take it from size 1 to 2
+        if (size == 2) {
+            tail = newNode.getNext();
+        }
+    }
+
+    /**
+     * Adds the element to the back of the list.
+     * <p>
+     * Method should run in O(1) time.
+     *
+     * @param data the data to add to the back of the list
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addToBack(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Can't add null data!");
+        }
+        size++;
+        var newNode = new SinglyLinkedListNode<T>(data);
+        // special case: size was 0 before (now 1)
+        if (size == 1){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail.setNext(newNode);
+        // head pointer edge case, for adding to a list to take it from size 1 to 2
+        if (size == 2) {
+            head = tail;
+        }
+        tail = newNode;
+    }
+
+    /**
+     * Removes and returns the first data of the list.
+     * <p>
+     * Method should run in O(1) time.
+     *
+     * @return the data formerly located at the front of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromFront() {
+        if (size == 0) {
+            throw new NoSuchElementException("List empty!");
+        }
+        size--;
+        var ret = head;
+        head = head.getNext();
+        // edge cases for tail pointer
+        // size now 0: tail should be null
+        // size now 1: tail should be == head
+        if (size == 0 || size == 1) {
+            tail = head;
+        }
+        return ret.getData();
+    }
+
+    /**
+     * Removes and returns the last data of the list.
+     * <p>
+     * Method should run in O(n) time.
+     *
+     * @return the data formerly located at the back of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromBack() {
+        if (size == 0) {
+            throw new NoSuchElementException("List empty!");
+        }
+        size--;
+        // edge case going from size 1 to 0:
+        // return the one element and set tail / head to null
+        if( size == 0) {
+            var ret = head;
+            head = null;
+            tail = null;
+            return ret.getData();
+        }
+
+        // traverse list until we are at the penultimate element
+        var current = head;
+        while( current.getNext().getNext() != null) {
+            current = current.getNext();
+        }
+        var ret = current.getNext();
+        tail = current;
+        current.setNext(null);
+
+        return ret.getData();
+    }
+
+    /**
+     * Returns the head node of the list.
+     * <p>
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the head of the list
+     */
+    public SinglyLinkedListNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the list.
+     * <p>
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the tail of the list
+     */
+    public SinglyLinkedListNode<T> getTail() {
+        // DO NOT MODIFY THIS METHOD!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the list.
+     * <p>
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the list
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
